cb881da56bdfbd4ceb5ce808b195c6c64f546f3a
ADMIN <qwertysdfg77@gmail.com>
2025-01-30 16:42:25 +0300

    -

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,19 +17,41 @@ namespace couples
     public partial class MainWindow : Window
     {
 
-   
-
-       
 
         public MainWindow()
         {
             InitializeComponent();
 
+            List<Button> buttons = GetButtonsFromGrid(DeskGrid);
+            int n = 0;
+            foreach(var button in buttons)
+            {
+                button.Content = $"it's {n} button";
+                n++;
+             
+            }
         }
 
         private void BT_Zero_Zero(object sender, RoutedEventArgs e)
         {
-            BT_Z_Z.Source = backSide;
+           
+        }
+
+
+
+        private List<Button> GetButtonsFromGrid(Grid grid)
+        {
+            List<Button> buttons = new List<Button>();
+
+            foreach (UIElement child in grid.Children)
+            {
+                if (child is Button button)
+                {
+                    buttons.Add(button);
+                }
+            }
+
+            return buttons;
         }
     }
 }
