c5fdeef4989b6aef0ecc57871fb57c40f7ea6dca
ADMIN <qwertysdfg77@gmail.com>
2025-02-21 09:06:06 +0300

    -

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System.ComponentModel;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-using System.Runtime.CompilerServices;
+using System.Windows.Threading;
 
 namespace couples
 {
@@ -22,13 +16,18 @@ namespace couples
 
         private readonly List<Button> _buttons;
         private readonly DeskHandler _handler;
+        [Required]
+        private DispatcherTimer _timer;
+        private int _remainingTime = 15;
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
             _buttons = GetButtonList();
 
             _handler = new DeskHandler(_buttons);
+
 
         }
 
@@ -45,93 +44,112 @@ namespace couples
             return _bt;
         }
 
-    }
-
-    public class Card : INotifyPropertyChanged
-    {
-
-        private Button _button { get; set; }
-
-        public readonly Image BackSide = new() { Source = (new BitmapImage(new Uri("pack://application:,,,/couples;component/image/back.jpg"))) };
-
-        public Image FrontSide { get; set; }
-
-        public int _cardId { get; private set; }
-
-        private bool _IsOpent;
-
-        public bool IsOpen
+        private void InitializeTimer()
         {
-            get { return _IsOpent; }
-            set
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            _timer.Tick += TimerTick;
+            _timer.Start();
+        }
+
+        private void TimerTick(object? sender, EventArgs e)
+        {
+            if (_remainingTime > 0)
             {
-                if (value != _IsOpent)
+                _remainingTime--;
+                TimerLable.Text = _remainingTime.ToString();
+            }
+            else
+            {
+                _timer.Stop();
+                TimerLable.Text = "you lose";
+            }
+
+        }
+
+        public class Card : INotifyPropertyChanged
+        {
+
+            private Button _button { get; set; }
+
+            public readonly Image BackSide = new() { Source = (new BitmapImage(new Uri("pack://application:,,,/couples;component/image/back.jpg"))) };
+
+            public Image FrontSide { get; set; }
+
+            public int _cardId { get; private set; }
+
+            private bool _IsOpent;
+
+            public bool IsOpen
+            {
+                get { return _IsOpent; }
+                set
                 {
-                    _IsOpent = value;
-                     OnPropertyChanged(nameof(IsOpen));
+                    if (value != _IsOpent)
+                    {
+                        _IsOpent = value;
+                        OnPropertyChanged(nameof(IsOpen));
+                    }
+
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public Card(Button button, int _cardId, Image FrontSide)
+            {
+                _button = button;
+                this.FrontSide = FrontSide;
+                _button.Click += ButtonClik;
+                this._cardId = _cardId;
+
+            }
+
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+            private void ButtonClik(object sender, RoutedEventArgs e)
+            {
+                IsOpen = IsOpen != true;
+                SetActualImage(BackSide, FrontSide);
+                if (IsOpen)
+                {
+                    _button.IsEnabled = false;
+                }
+            }
+
+            public void SetActualImage(Image backSide, Image frontSide)
+            {
+                if (IsOpen == false)
+                    _button.Content = backSide;
+                else
+                    _button.Content = frontSide;
+            }
+
+            public void EnableCards(bool enable)
+            {
+                if (!enable)
+                {
+                    _button.Visibility = Visibility.Hidden;
+                    _button.IsEnabled = false;
+                }
+
+                else
+                {
+                    _button.Visibility = Visibility.Visible;
+                    _button.IsEnabled = true;
                 }
 
             }
-        }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public Card(Button button, int _cardId, Image FrontSide)
-        {
-            _button = button;
-            this.FrontSide = FrontSide;
-            _button.Click += ButtonClik;
-            this._cardId = _cardId; 
 
         }
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+
+        public class DeskHandler
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private void ButtonClik(object sender, RoutedEventArgs e)
-        {
-            IsOpen = IsOpen != true;
-            SetActualImage(BackSide, FrontSide);
-            if (IsOpen)
-            {
-                _button.IsEnabled = false;
-            }
-        }
-
-        public void SetActualImage(Image backSide, Image frontSide)
-        {
-            if (IsOpen == false)
-                _button.Content = backSide;
-            else
-                _button.Content = frontSide;
-        }
-
-        public void EnableCards(bool enable)
-        {
-            if (!enable)
-            {
-                _button.Visibility = Visibility.Hidden;
-                _button.IsEnabled = false;
-            }
-
-            else
-            {
-                _button.Visibility = Visibility.Visible;
-                _button.IsEnabled = true;
-            }
-           
-           }
-                
-
-    }
-
-
-    public class DeskHandler 
-    {
-       
-        Dictionary<int, string> FrontSideImage = new()
+            private Dictionary<int, string> FrontSideImage = new()
         {
             {0, "pack://application:,,,/couples;component/image/1.jpg"},
             {1, "pack://application:,,,/couples;component/image/2.jpg"},
@@ -143,89 +161,90 @@ namespace couples
             {7, "pack://application:,,,/couples;component/image/8.jpg"},
         };
 
-        private List<Card> cards = [];
+            private List<Card> cards = [];
 
-        private int _countOpenCard;
+            private int _countOpenCard;
 
-        public int CountOpenCard
-        {
-            get { return _countOpenCard; }
-            set
+            public int CountOpenCard
             {
-                _countOpenCard = value;
-                if(_countOpenCard == 2)
+                get { return _countOpenCard; }
+                set
                 {
-                    checkedOpenCards();
-                    _countOpenCard = 0;
-                }
-            }
-        }
-
-        private void checkedOpenCards()
-        {
-            List<Card> couples = [];
-
-            foreach (Card card in cards)
-            {
-                if (card.IsOpen)
-                {
-                    couples.Add(card);
-                }
-            }
-
-            if(couples.Count == 2)
-            {
-                if (couples[0]._cardId == couples[1]._cardId)
-                {
-                    foreach(var item in couples)
+                    _countOpenCard = value;
+                    if (_countOpenCard == 2)
                     {
-                        item.EnableCards(false);
-                        item.IsOpen = false;
-                    }
-                    MessageBox.Show("you found couples");
-                }
-                else
-                {
-                    foreach(var item in couples)
-                    {
-                        item.IsOpen = false;
-                        item.SetActualImage(item.BackSide,item.FrontSide);
-                        item.EnableCards(true);
+                        checkedOpenCards();
+                        _countOpenCard = 0;
                     }
                 }
             }
-        }
 
-        public DeskHandler(List<Button> buttons)
-        {
-            int temp = 0;
-
-            for (int i = 0; i < buttons.Count; i++)
+            private void checkedOpenCards()
             {
-                cards.Add(new Card(buttons[i], temp, new Image { Source = new BitmapImage(new Uri(FrontSideImage[temp])) }));
-                buttons[i].Content = cards[i].BackSide;
-                cards[i].PropertyChanged += Card_PropertyChanged;
-                if (i % 2 != 0)
-                    temp++;
-            }
+                List<Card> couples = [];
 
-        }
-        private void Card_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == nameof(Card.IsOpen))
-            {
-                var temp = sender as Card;
-                if(temp != null)
+                foreach (Card card in cards)
                 {
-                    if (temp.IsOpen == true)
-                        CountOpenCard++;
+                    if (card.IsOpen)
+                    {
+                        couples.Add(card);
+                    }
                 }
-              
 
- 
+                if (couples.Count == 2)
+                {
+                    if (couples[0]._cardId == couples[1]._cardId)
+                    {
+                        foreach (var item in couples)
+                        {
+                            item.EnableCards(false);
+                            item.IsOpen = false;
+                        }
+                        MessageBox.Show("you found couples");
+                    }
+                    else
+                    {
+                        foreach (var item in couples)
+                        {
+                            item.IsOpen = false;
+                            item.SetActualImage(item.BackSide, item.FrontSide);
+                            item.EnableCards(true);
+                        }
+                    }
+                }
             }
-        }
 
+            public DeskHandler(List<Button> buttons)
+            {
+                int temp = 0;
+
+                for (int i = 0; i < buttons.Count; i++)
+                {
+                    cards.Add(new Card(buttons[i], temp, new Image { Source = new BitmapImage(new Uri(FrontSideImage[temp])) }));
+                    buttons[i].Content = cards[i].BackSide;
+                    cards[i].PropertyChanged += Card_PropertyChanged;
+                    if (i % 2 != 0)
+                        temp++;
+                }
+
+            }
+            private void Card_PropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (e.PropertyName == nameof(Card.IsOpen))
+                {
+                    var temp = sender as Card;
+                    if (temp != null)
+                    {
+                        if (temp.IsOpen == true)
+                            CountOpenCard++;
+                    }
+
+
+
+                }
+            }
+
+        }
     }
 }
 
